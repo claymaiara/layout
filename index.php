6713b3cb9a1f0521970000f37735cfc3627cdefe
marciomrs4 <marciomrs4@hotmail.com>
2014-04-13 19:43:04 -0300

    Adcionado primeiro commit

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-<meta charset="UTF-8">
+<meta charset="ISO-8859-1">
 <title>Home</title>
 <link rel="stylesheet" href="css/bootstrap.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,6 +10,7 @@
 
 	<!--  PESQUISA -->
 	<div class="container-fluid">
+
 		<nav class="navbar navbar-inverse  navbar-fixed-top" role="navigation">
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle" data-toggle="collapse"
@@ -18,8 +19,11 @@
 						class="icon-bar"></span> <span class="icon-bar"></span> <span
 						class="icon-bar"></span>
 				</button>
+				
 				<div class="btn-group">
-					<button class="btn btn-default btn-lg">Ceadis</button>
+					<button class="btn btn-default btn-lg">
+						<span class="glyphicon glyphicon-th-large"></span> Ceadis
+					</button>
 					<button data-toggle="dropdown"
 						class="btn btn-default btn-lg dropdown-toggle">
 						<span class="caret"></span>
@@ -28,63 +32,117 @@
 						<li><a href="#">Action</a></li>
 						<li><a href="#">Another action</a></li>
 						<li class="divider"></li>
-						<li><a href="#">Separated link</a></li>
+						<li><a href="#"><span class="glyphicon glyphicon-user"></span> Alterar Senha</a></li>
 					</ul>
-
 				</div>
+				
+				
 			</div>
 
 			<div class="nav navbar-left collapse navbar-collapse">
 				<p class="navbar-text">SGP - SISTEMA DE GESTAO PORTARIA</p>
 			</div>
 			<div class="nav navbar-right collapse navbar-collapse">
-				<button class="btn btn-default btn-lg ">Login</button>
+				<button class="btn btn-default btn-lg ">
+					<span class="glyphicon glyphicon-off"></span> Sair
+				</button>
 			</div>
 		</nav>
+	
 		<nav class="navbar navbar-default" role="navigation"></nav>
 
+<div class="panel panel-primary">
+  	<div class="panel-heading">
+  		<h3 class="panel-title">Chamados</h3>
+  	</div>
+  <div class="panel-body">
+	
+<!-- Painel de ações -->
 
-		<div class="row-fluid">
-			<div class="col-sm-12 well">
-				<form method="post" id="meuform" action="" class="form-inline">
-					<fieldset>
-						<legend>Pesquisar</legend>
-						<div class="form-group">
-							<label for="validade-cartao">Data Inicial</label> <input
-								type="month" class="form-control" id="validade-cartao"
-								name="validade-cartao">
-						</div>
-						<div class="form-group">
-							<label for="validade-cartao">Data Final</label> <input
-								type="month" class="form-control" id="validade-cartao"
-								name="validade-cartao">
-						</div>
-						<div class="form-group">
-							<label for="nome">Nome </label> <input type="text"
-								class="form-control" id="nome" name="nome" autofocus required>
-						</div>
-						<div class="form-group">
-							<label for="email">Email</label>
-						</div>
-						<div class="input-group">
-							<span class="input-group-addon">@</span> <input type="email"
-								class="form-control" id="email" name="email"
-								placeholder="email@exemplo.com">
-						</div>
+		<ul class="nav nav-pills">
+		  	<li>						
+		  		<button type="submit" class="btn btn-primary pull-right">
+					<span class="glyphicon glyphicon-plus"></span> Novo
+				</button>
+			</li>
+		  <li>
+		  	<button type="submit" class="btn btn-primary pull-right">
+				<span class="glyphicon glyphicon-list"></span> Listar
+			</button></li>
+		  <li class="dropdown">
+				<div class="btn-group">
+				  <button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-list-alt"></span> Relatório</button>
+				  <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+				    <span class="caret"></span>
+				    <span class="sr-only">Toggle Dropdown</span>
+				  </button>
+				  <ul class="dropdown-menu" role="menu">
+				    <li><a href="#"><span class="glyphicon glyphicon-list-alt"></span> Action</a></li>
+				    <li><a href="#"><span class="glyphicon glyphicon-list-alt"></span> Another action</a></li>
+				    <li><a href="#"><span class="glyphicon glyphicon-list-alt"></span> Something else here</a></li>
+				    <li class="divider"></li>
+				    <li><a href="#"><span class="glyphicon glyphicon-list-alt"></span> Separated link</a></li>
+				  </ul>
+				</div>
+		  </li>
+		</ul>
 
-						<button type="submit" class="btn btn-primary btn-lg pull-right">
-							<span class="glyphicon glyphicon-thumbs-up"></span> Confirmar
-						</button>
-					</fieldset>
-				</form>
-			</div>
-		</div>
+	<!-- Fim Painel de ações -->
+  
+  </div>
+</div>
+		
+		
+		
+		<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">Pesquisa</h3>
+  </div>
+  <div class="panel-body">
+  
+  <div class="col-xs-6">
+		 <form class="form-horizontal" role="form">
+			  <div class="form-group col-xs-4 col-sm-4">
+			    <label class="sr-only" for="exampleInputEmail2">Email address</label>
+			    <input type="date" class="form-control" id="exampleInputEmail2" placeholder="data">
+			  </div>
+			  <div class="form-group col-xs-10 col-sm-4">
+			    <label class="sr-only" for="exampleInputPassword2">Password</label>
+			    <input type="text" class="form-control" id="exampleInputPassword2" placeholder="descricão">
+			  </div>
+			  
+  			  <div class="form-group col-xs-6 col-sm-4">
+					<select class="form-control">
+					  <option>Todos</option>
+					  <option>Opção teste de texto grande</option>
+					  <option>Opção teste de texto muito grande</option>
+					  <option>Opção teste de texto muito grande mesmo</option>
+					  <option>Opção 5</option>
+					</select>
+			  </div>
 
-		<div class="table-responsive">
+			  <div class="form-group col-xs-8">
+			  	<button type="submit" class="btn btn-primary "><span class="glyphicon glyphicon-search"></span> Pesquisar</button>
+			  </div>
+		</form>
+	</div>	
+	
+  </div>
+</div>
+
+
+
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">Resultado(s)</h3>
+  </div>
+  <div class="panel-body">
+
+
 			<table class="table table-striped table-bordered table-condensed table-hover">
 				<thead>
 					<tr class="info">
-						<th>Row</th>
+						<th>#</th>
 						<th>Bill</th>
 						<th>Payment Date</th>
 						<th>Payment Status</th>
@@ -92,7 +150,20 @@
 				</thead>
 				<tbody>
 					<tr>
-						<td>1</td>
+						<td>
+						<div class="btn-group">
+  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+    Opções 
+    <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu" role="menu">
+    <li><a href="#"><span class="glyphicon glyphicon-pencil"></span> Editar</a></li>
+    <li><a href="#"><span class="glyphicon glyphicon-search"></span> Visualizar</a></li>
+    <li><a href="#"><span class="glyphicon glyphicon-file"></span> Assentamento</a></li>
+    <li><a href="#"><span class="glyphicon glyphicon-download-alt"></span> Exportar</a></li>    
+  </ul>
+  </div>
+						</td>
 						<td>Credit Card</td>
 						<td>04/08/2013</td>
 						<td>Call in to confirm</td>
@@ -214,8 +285,11 @@
 
 				</tbody>
 			</table>
-		</div>
-	</div>
+
+			</div>
+		</div>			
+
+
 	<footer>
 
 
@@ -223,10 +297,16 @@
 			role="navigation">
 			<p class="navbar-text">..: SGA - Sistema de Gestão de Atividades
 				[Tecnologia da Informação] :..</p>
+							<div class="nav navbar-right collapse navbar-collapse">
+				<button class="btn btn-default btn-lg">
+					<span class="glyphicon glyphicon-user"></span> Usuário
+				</button>
+			</div>
 		</nav>
 		<nav class="navbar navbar-default" role="navigation"></nav>
 	</footer>
 
+</div>
 
 
 
