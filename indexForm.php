9cb0302aa93ec28f09c215a704eaaf7b574d86b9
marciomrs4 <marciomrs4@hotmail.com>
2014-04-16 10:56:33 -0300

    Versao com Formulario de Busca e Cadastro

--- indexForm.php
+++ indexForm.php
@@ -107,29 +107,54 @@
 
 		<div class="panel panel-default">
 			<div class="panel-heading">
-				<h3 class="panel-title">Pesquisar</h3>
+				<h3 class="panel-title">Novo</h3>
 			</div>
 			<div class="panel-body">
 		
-				<form class="form-inline" role="form">
+				<form class="form-horizontal" role="form">
 
 				<div class="form-group">
-				    <label for="inputEmail3" class="sr-only">Email</label>
+				    <label for="inputEmail3" class="col-sm-1 control-label">Email</label>
 				    <div class="col-sm-4">
 				      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
 				    </div>
 				  </div>
 				
 				  <div class="form-group">
-				    <label for="inputPassword3" class="sr-only">Password</label>
+				    <label for="inputPassword3" class="col-sm-1 control-label">Password</label>
 				    <div class="col-sm-4">
 				      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
 				    </div>
 				  </div>
 				  
 				  <div class="form-group">
-			    	<label for="" class="col-sm-4">Selecione</label>				        
+				    <div class="col-sm-offset-1 col-sm-2">
+				      <div class="checkbox">
+				        <label>
+				          <input type="checkbox"> Remember me
+				        </label>
+				      </div>
+				    </div>
+				  
+				    <div class="col-sm-1">
+				      <div class="checkbox">
+				        <label>
+				          <input type="checkbox"> Teste
+				        </label>
+				      </div>
+				    </div>
+				  </div>
+				  
+				  <div class="form-group">
+			    	<label for="" class="col-sm-1 control-label">Descrição</label>				        
 				    <div class="col-sm-4">
+				          <textarea class="form-control" rows="3"></textarea>
+				    </div>
+				  </div>
+				  
+				  <div class="form-group">
+			    	<label for="" class="col-sm-1 control-label">Selecione</label>				        
+				    <div class="col-sm-2">
 						<select class="form-control">
 						  <option></option>
 						  <option>allsjh</option>
@@ -142,12 +167,10 @@
 				  </div>
 				  
 				  
-							  
-				  
 				  <div class="form-group">
 				    <div class="col-sm-offset-1 col-sm-1">
 				      <button type="submit" class="btn btn-primary">
-				      	<span class="glyphicon glyphicon-search"></span> Pesquisar
+				      	<span class="glyphicon glyphicon-floppy-saved"></span> Salvar
 				      </button>
 				    </div>
 				  </div>
@@ -157,171 +180,6 @@
 		
 			</div>
 		</div>
-
-
-
-		<div class="panel panel-default">
-			<div class="panel-heading">
-				<h3 class="panel-title">Resultado(s)</h3>
-			</div>
-			<div class="panel-body">
-
-
-				<table
-					class="table table-striped table-bordered table-condensed table-hover">
-					<thead>
-						<tr class="info">
-							<th>#</th>
-							<th>Bill</th>
-							<th>Payment Date</th>
-							<th>Payment Status</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td class="col-md-1">
-								<div class="btn-group">
-									<button type="button" class="btn btn-default dropdown-toggle"
-										data-toggle="dropdown">
-										Opções <span class="caret"></span>
-									</button>
-									<ul class="dropdown-menu" role="menu">
-										<li><a href="#"><span class="glyphicon glyphicon-pencil"></span>
-												Editar</a></li>
-										<li><a href="#"><span class="glyphicon glyphicon-search"></span>
-												Visualizar</a></li>
-										<li><a href="#"><span class="glyphicon glyphicon-file"></span>
-												Assentamento</a></li>
-										<li><a href="#"><span class="glyphicon glyphicon-download-alt"></span>
-												Exportar</a></li>
-									</ul>
-								</div>
-							</td>
-							<td>Credit Card</td>
-							<td>04/08/2013</td>
-							<td>Call in to confirm</td>
-						</tr>
-						<tr>
-							<td>2</td>
-							<td>Water</td>
-							<td>01/08/2013</td>
-							<td>Paid</td>
-						</tr>
-						<tr>
-							<td>3</td>
-							<td>Electricity</td>
-							<td>03/08/2013</td>
-							<td>Pending</td>
-						</tr>
-						<tr>
-							<td>4</td>
-							<td>Telephone</td>
-							<td>06/08/2013</td>
-							<td>Due</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>Credit Card</td>
-							<td>04/08/2013</td>
-							<td>Call in to confirm</td>
-						</tr>
-						<tr>
-							<td>2</td>
-							<td>Water</td>
-							<td>01/08/2013</td>
-							<td>Paid</td>
-						</tr>
-						<tr>
-							<td>3</td>
-							<td>Electricity</td>
-							<td>03/08/2013</td>
-							<td>Pending</td>
-						</tr>
-						<tr>
-							<td>4</td>
-							<td>Telephone</td>
-							<td>06/08/2013</td>
-							<td>Due</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>Credit Card</td>
-							<td>04/08/2013</td>
-							<td>Call in to confirm</td>
-						</tr>
-						<tr>
-							<td>2</td>
-							<td>Water</td>
-							<td>01/08/2013</td>
-							<td>Paid</td>
-						</tr>
-						<tr>
-							<td>3</td>
-							<td>Electricity</td>
-							<td>03/08/2013</td>
-							<td>Pending</td>
-						</tr>
-						<tr>
-							<td>4</td>
-							<td>Telephone</td>
-							<td>06/08/2013</td>
-							<td>Due</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>Credit Card</td>
-							<td>04/08/2013</td>
-							<td>Call in to confirm</td>
-						</tr>
-						<tr>
-							<td>2</td>
-							<td>Water</td>
-							<td>01/08/2013</td>
-							<td>Paid</td>
-						</tr>
-						<tr>
-							<td>3</td>
-							<td>Electricity</td>
-							<td>03/08/2013</td>
-							<td>Pending</td>
-						</tr>
-						<tr>
-							<td>4</td>
-							<td>Telephone</td>
-							<td>06/08/2013</td>
-							<td>Due</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>Credit Card</td>
-							<td>04/08/2013</td>
-							<td>Call in to confirm</td>
-						</tr>
-						<tr>
-							<td>2</td>
-							<td>Water</td>
-							<td>01/08/2013</td>
-							<td>Paid</td>
-						</tr>
-						<tr>
-							<td>3</td>
-							<td>Electricity</td>
-							<td>03/08/2013</td>
-							<td>Pending</td>
-						</tr>
-						<tr>
-							<td>4</td>
-							<td>Telephone</td>
-							<td>06/08/2013</td>
-							<td>Due</td>
-						</tr>
-
-					</tbody>
-				</table>
-
-			</div>
-		</div>
-
 
 		<footer>
 			<nav class="navbar navbar-default navbar-fixed-bottom"
